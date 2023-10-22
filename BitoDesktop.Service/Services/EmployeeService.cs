f2f2d5e9ab79f5748da76ca752273e1834c96277
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-10-22 17:18:07 +0500

    Added EmployeeService ProductService

---
+++ BitoDesktop.Service/Services/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using BitoDesktop.Data.Repositories.CustomerP;
+using BitoDesktop.Data.Repositories.Hr;
+using BitoDesktop.Data.Repositories.WarehouseP;
+using BitoDesktop.Domain.Entities.CustomerP;
+using BitoDesktop.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitoDesktop.Service.Services
+{
+    public class EmployeeService
+    {
+        private readonly CustomerRepository customerRepository;
+
+        public EmployeeService()
+        {
+            customerRepository = new CustomerRepository();
+        }
+
+        public async Task<Customer> GetById(string customerId, string organizationId)
+        {
+            var customer = await customerRepository
+                .GetById(customerId, organizationId, true, true);
+
+            if (customer == null)
+                throw new MarketException(404, "Customer not found");
+
+            return customer;
+        }
+
+        public async Task<IEnumerable<CustomerAddress>> GetCustomers(string organizationId)
+        {
+            var customers = await customerRepository.GetCustomers(100,100,100,100,organizationId);
+
+            if (customers == null)
+                throw new MarketException(404, "Customer not found");
+
+            return customers;
+        }
+    }
+}
---
+++ BitoDesktop.Service/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using BitoDesktop.Data.Repositories.WarehouseP;
+using BitoDesktop.Domain.Entities.Products;
+using BitoDesktop.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitoDesktop.Service.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository productRepository;
+        public ProductService()
+        {
+            productRepository = new ProductRepository();
+        }
+
+        public async Task<Product> GetById(string productId, string organizationId,string warehouseId,string priceId)
+        {
+            var product = await productRepository
+                .GetById(productId,organizationId,warehouseId, priceId,true,true,false);
+
+            if (product == null)
+                throw new MarketException(404,"Product not found");
+            
+            return product;
+        }
+        public async Task<Product> GetByBarcode(string barcode, string organizationId,string warehouseId,string priceId)
+        {
+            var product = await productRepository
+                .GetById(barcode,organizationId,warehouseId, priceId,true,true,false);
+
+            if (product == null)
+                throw new MarketException(404,"Product not found");
+            
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts(string value, string organizationId, string warehouseId, string priceId)
+        {
+            var product = await productRepository
+                .GetMultiple(value, organizationId, warehouseId, priceId, true, true, false);
+
+            if (product == null)
+                throw new MarketException(404, "Product not found");
+
+            return product;
+        }
+    }
+}
