c96c630cf024468bfbdeb320dc828aaadc66f84a
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-11-04 10:48:12 +0500

    some changes

--- BitoDesktop.WPF/Pages/PosPage.xaml.cs
+++ BitoDesktop.WPF/Pages/PosPage.xaml.cs
@@ -3,6 +3,7 @@ using BitoDesktop.Service.Http;
 using BitoDesktop.Service.Services;
 using BitoDesktop.WPF.Controllers.Pos;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace BitoDesktop.WPF.Pages
@@ -25,42 +26,51 @@ namespace BitoDesktop.WPF.Pages
 
         private async void SearchProduct_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(SearchProduct.Text))
-            {
-                ProductSearchItems.Items.Clear();
-                var warehouseId = await configurationService.GetWarehouse();
-                var priceId = await configurationService.GetPrice();
-                var organizationId = await configurationService.GetOrganization();
+            //if (!string.IsNullOrEmpty(SearchProduct.Text))
+            //{
+            //    ProductSearchItems.Items.Clear();
+            //    var warehouseId = await configurationService.GetWarehouse();
+            //    var priceId = await configurationService.GetPrice();
+            //    var organizationId = await configurationService.GetOrganization();
 
-                var products = await productService.GetProducts(SearchProduct.Text, organizationId, warehouseId, priceId);
+            //    var products = await productService.GetProducts(SearchProduct.Text, organizationId, warehouseId, priceId);
 
-                foreach (var product in products)
-                {
-                    ProductSearchController productSearchController = new ProductSearchController();
-                    productSearchController.NameTxt.Text = product.Name;
-                    productSearchController.BarcodeTxt.Text = product.Barcode;
-                    productSearchController.CapacityTxt.Text = product.WarehouseAmount.ToString();
-                    ProductSearchItems.Items.Add(productSearchController);
-                }
-            }
-            else 
-            {
-                ProductSearchItems.Items.Clear();
-            }
+            //    foreach (var product in products)
+            //    {
+            //        ProductSearchController productSearchController = new ProductSearchController();
+            //        productSearchController.NameTxt.Text = product.Name;
+            //        productSearchController.BarcodeTxt.Text = product.Barcode;
+            //        productSearchController.CapacityTxt.Text = product.WarehouseAmount.ToString();
+            //        ProductSearchItems.Items.Add(productSearchController);
+            //    }
+            //}
+            //else 
+            //{
+            //    ProductSearchItems.Items.Clear();
+            //}
         }
 
-        private async void SearchCustomer_TextChanged(object sender, TextChangedEventArgs e)
+        private void SearchCustomer_TextChanged(object sender, TextChangedEventArgs e)
         {
             SearchItems.Items.Clear();
-            var customers = await customerService.GetCustomers(Client.OrganizationId,SearchCustomer.Text);
 
-            foreach (var customer in customers) 
+            string query = SearchCustomer.Text;
+            var task = new Task(async () =>
             {
-                CustomerSearchItemController customerController = new CustomerSearchItemController();
-                customerController.CustomerNaeTxt.Text = customer.Name;
-                customerController.CustomerPhoneNumber.Text = customer.PhoneNumber;
-                SearchItems.Items.Add(customerController);
-            }
+                var customers = await customerService.GetCustomers(Client.OrganizationId, query);
+
+                foreach (var customer in customers)
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        CustomerSearchItemController customerController = new CustomerSearchItemController();
+                        customerController.CustomerNaeTxt.Text = customer.Name;
+                        customerController.CustomerPhoneNumber.Text = customer.PhoneNumber;
+                        SearchItems.Items.Add(customerController);
+                    });
+                }
+            });
+            task.Start();
         }
 
         private void SearchCustomer_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
